73c6adb784d5268ab3b48026d5b25952f6d84770
federica <fedhere@gmail.com>
2015-12-15 11:52:31 -0500

    add setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='sparklpy',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='Federica B. Bianco',
+      author_email='fedhere@gmail.com',
+      url='https://github.com/fedhere/sparklpy',
+      py_modules=['sparkleme'],
+     )
